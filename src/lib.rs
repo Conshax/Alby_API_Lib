7615465062a8f75a4a134ac290558d2eda2aa51a
Alwin Stockinger <alwin@conshax.com>
2023-01-10 12:52:06 +0100

    refresh token implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use reqwest::{self, header};
 use reqwest::header::HeaderMap;
 
@@ -7,8 +9,16 @@ use thiserror::Error;
 
 use serde_json;
 
+pub type Result<T> = std::result::Result<T, Error>;
+
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("Missing Auth")]
+    MissingAuth,
+
+    #[error("Missing Refresh Token")]
+    MissingRefreshToken,
+
     #[error("Invalid access token")]
     InvalidAccessToken,
 
@@ -17,16 +27,39 @@ pub enum Error {
 
     #[error("Parsing Error {msg:?}")]
     ParsingError { msg: String },
+
+    #[error("Authorization Error")]
+    AuthError,
+
+    #[error("Alby Error")]
+    AlbyError(String),
+}
+
+struct Auth {
+    pub client_id: String,
+    pub client_secret: String,
 }
 pub struct Client {
     pub client: reqwest::Client,
     pub access_token: String,
     pub refresh_token: Option<String>,
+    auth: Option<Auth>,
 }
 
 
 impl Client {
-    pub fn from_access_token(access_token: String) -> Result<Self, Error> {
+    pub fn set_refresh_token(&mut self, refresh_token: String) {
+        self.refresh_token = Some(refresh_token);
+    }
+
+    pub fn set_auth(&mut self, client_id: String, client_secret: String) {
+        self.auth = Some(Auth {
+            client_id,
+            client_secret,
+        });
+    }
+
+    pub fn from_access_token(access_token: String) -> Result<Self> {
         let mut headers = HeaderMap::new();
         let mut auth_value = header::HeaderValue::from_str(&format!("Bearer {}", access_token)).map_err(|_| Error::InvalidAccessToken)?;
         auth_value.set_sensitive(true);
@@ -41,17 +74,47 @@ impl Client {
             client,
             access_token,
             refresh_token: None,
+            auth: None, 
         })
     }
 
-    pub async fn get_value4value(&self) -> Result<Value4ValueResponse, Error> {
+    pub async fn refresh_token(&self) -> Result<()> {
+        let auth = self.auth.as_ref().ok_or(Error::MissingAuth)?;
+        let refresh_token = self.refresh_token.as_ref().ok_or(Error::MissingRefreshToken)?;
+
+
+        let form: HashMap<&str, &str> = HashMap::from_iter(vec![
+            ("grant_type", "refresh_token"),
+            ("refresh_token", refresh_token),
+        ]);
+
+        let resp = self.client.post("https://api.getalby.com/oauth/token")
+            .basic_auth(&auth.client_id, Some(&auth.client_secret))
+            .form(&form)
+            .header(header::CONTENT_TYPE, "multipart/form-data")
+            .send()
+            .await
+            .map_err(|e| Error::RequestError(e))?;
+
+        if resp.status() == 401 {
+            return Err(Error::AuthError)
+        }
+        if resp.status() != 200 {
+            let error = resp.error_for_status().map_err(|e| Error::RequestError(e))?.text().await.map_err(|e| Error::RequestError(e))?;
+            return Err(Error::AlbyError(error))
+        }
+
+        return Ok(());
+    }
+
+    pub async fn get_value4value(&self) -> Result<Value4ValueResponse> {
         let resp = self.client.get("https://api.getalby.com/user/value4value")
             .send()
             .await
             .map_err(|e| Error::RequestError(e))?;
 
         if resp.status() == 401 {
-            return Err(Error::InvalidAccessToken)
+            return Err(Error::AuthError)
         }
 
         let resp = resp.bytes()
